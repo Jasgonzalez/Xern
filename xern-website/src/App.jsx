6b9d11065c952fe9332a868c5e0b26946d726a5e
Jason Gonzalez <jegonzalez1111@gmail.com>
2024-09-14 19:34:05 -0400

    React files

---
+++ xern-website/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header'
+import Footer from './footer';
+
+import './App.css'
+
+function App() {
+  
+
+  return(
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App
